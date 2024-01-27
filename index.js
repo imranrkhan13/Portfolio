cd13c084c1eb31c46bfaed6e64bf62cfbadb8254
Imran Khan <99342205+imranrkhan13@users.noreply.github.com>
2024-01-27 21:16:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+const typed = new Typed('#element', {
+    strings: ['{Web developer', '{Designer ', '{Freelancer'],
+    typeSpeed: 70,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true,
+    cursorChar: "}"
+});
+
+
